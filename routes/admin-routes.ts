0d038da8b673aefb4454f2b056d51821ce4359a7
Steven Gutierrez <stematgut@gmail.com>
2020-05-09 22:51:58 -0700

    commented in various api routes in files in new routes folder

---
+++ routes/admin-routes.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const router = express.Router();
+
+// '/users' PATCH
+// userId & all fields to update must be incl.
+
+export default router;
---
+++ routes/authentication.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const router = express.Router();
+
+// login POST
+
+export default router;
