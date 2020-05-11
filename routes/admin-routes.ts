144b02b80a167a2f1dfbdd2b2e10bd9486ba5905
Steven Gutierrez <stematgut@gmail.com>
2020-05-11 13:45:58 -0700

    Added in skeletal router-endpoints for the required routes in the routes folder

--- routes/admin-routes.ts
+++ routes/admin-routes.ts
@@ -2,7 +2,10 @@ import express from 'express';
 
 const router = express.Router();
 
-// '/users' PATCH
+// @'/users' PATCH
 // userId & all fields to update must be incl.
+router.patch('/users', function (req, res) {
+  // UPDATE USER valuestoupdate WHERE userId = req.body.userId
+});
 
 export default router;
