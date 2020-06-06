2cccf5fd9ba071885c32db7e688d988e96c2c916
Steven Gutierrez <stematgut@gmail.com>
2020-06-06 14:43:06 -0700

    Added logout route

--- src/routes/authentication.ts
+++ src/routes/authentication.ts
@@ -23,4 +23,18 @@ router.post('/login', async (req: Request, res: Response) => {
   }
 });
 
+// @route   GET /logout
+router.get('/logout', function (req, res, next) {
+  if (req.session) {
+    // delete session object
+    req.session.destroy(function (err) {
+      if (err) {
+        return next(err);
+      } else {
+        return res.redirect('/landing');
+      }
+    });
+  }
+});
+
 export default router;
